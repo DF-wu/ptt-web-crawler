47ec905788885104c12b7bac2fedd99ee0948b50
dfder <a2470abc@gmail.com>
2021-12-24 23:56:56 +0800

    change headers

--- PttWebCrawler/crawler.py
+++ PttWebCrawler/crawler.py
@@ -28,14 +28,14 @@ if sys.version_info[0] < 3:
 fake_useragent = UserAgent()
 
 headers = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
-    "Accept-Encoding": "gzip, deflate, br", 
+    "Accept": "*", 
+    # "Accept-Encoding": "gzip, deflate, br", 
     "Accept-Language": "zh-TW,zh;q=0.9", 
     "Host": "ptt.cc",  #目標網站 
-    "Sec-Fetch-Dest": "document", 
-    "Sec-Fetch-Mode": "navigate", 
-    "Sec-Fetch-Site": "none", 
-    "Upgrade-Insecure-Requests": "1", 
+    # "Sec-Fetch-Dest": "document", 
+    # "Sec-Fetch-Mode": "navigate", 
+    # "Sec-Fetch-Site": "none", 
+    # "Upgrade-Insecure-Requests": "1", 
     "User-Agent": fake_useragent.random
         # "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36" #使用者代理
 }
