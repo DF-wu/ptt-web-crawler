c413182fd6183dfa185f33c24c5145ca547c2b2c
dfder <a2470abc@gmail.com>
2021-12-24 23:48:42 +0800

    fix UA

--- PttWebCrawler/crawler.py
+++ PttWebCrawler/crawler.py
@@ -25,6 +25,7 @@ if sys.version_info[0] < 3:
     VERIFY = False
     requests.packages.urllib3.disable_warnings()
 
+fake_useragent = UserAgent()
 
 headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
@@ -35,7 +36,7 @@ headers = {
     "Sec-Fetch-Mode": "navigate", 
     "Sec-Fetch-Site": "none", 
     "Upgrade-Insecure-Requests": "1", 
-    "User-Agent": UserAgent()
+    "User-Agent": fake_useragent.random
         # "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36" #使用者代理
 }
 
