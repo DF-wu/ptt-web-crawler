001c6ce158085472c0ee924454939333a33451e5
dfder <a2470abc@gmail.com>
2021-12-24 23:53:07 +0800

    change host

--- PttWebCrawler/crawler.py
+++ PttWebCrawler/crawler.py
@@ -31,7 +31,7 @@ headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
     "Accept-Encoding": "gzip, deflate, br", 
     "Accept-Language": "zh-TW,zh;q=0.9", 
-    "Host": "example.com",  #目標網站 
+    "Host": "ptt.cc",  #目標網站 
     "Sec-Fetch-Dest": "document", 
     "Sec-Fetch-Mode": "navigate", 
     "Sec-Fetch-Site": "none", 
